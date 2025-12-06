830f8bc23bd82344a7c183225120e5de1398585c
Mario Radich <marioradich@gmail.com>
2025-12-06 00:06:25 -0300

    ok

--- app.py
+++ app.py
@@ -5,11 +5,7 @@ app = Flask(__name__)
 # Routes
 @app.route('/')
 def index():
-    print("Hello, world!")
-    name = "Alice"
-    age = 30
-    print(f"My name is {name} and I am {age} years old.")
-    return render_template('index.html')
+ return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
