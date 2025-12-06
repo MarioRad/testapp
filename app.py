a63bd991f4ca2cc9685116b37c27b8aee3fab812
Mario Radich <marioradich@gmail.com>
2025-12-05 21:47:54 -0300

    Primer commit5

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 # Routes
 @app.route('/')
 def index():
+    print("Hello, world!")
+    name = "Alice"
+    age = 30
+    print(f"My name is {name} and I am {age} years old.")
     return render_template('index.html')
 
 if __name__ == '__main__':
